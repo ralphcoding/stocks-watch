49de53440985a5affdd5b2e06fafe04916de6af5
ralphcoding <ralphcoder@proton.me>
2026-01-31 23:01:01 +0000

    setup-auto: env + tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-sm">
+                <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+                    Tailwind Test Card
+                </div>
+                <p className="text-gray-700 mb-6">
+                    Testing current tailwind setup is working
+                </p>
+                <button className="w-full py-2 px-4 bg-blue-600 text-white rounded hover:bg-blue-700 transition">
+                    Hello world
+                </button>
+            </div>
+        </>
+    )
 }
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,56 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
+import path from 'path'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+    // Load env file based on mode in the current working directory
+    const env = loadEnv(mode, process.cwd(), '')
+    return {
+        base: `/${env.VITE_PROJECT_NAME}/`,
+        plugins: [
+            react(),
+            tailwindcss(),
+            VitePWA({
+                registerType: 'prompt',
+                injectRegister: false,
+
+                pwaAssets: {
+                    disabled: false,
+                    config: true,
+                },
+
+                manifest: {
+                    name: env.VITE_PROJECT_NAME,
+                    short_name: env.VITE_PROJECT_NAME,
+                    description: env.VITE_PROJECT_DESC,
+                    theme_color: '#ffffff',
+                },
+
+                workbox: {
+                    globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+                    cleanupOutdatedCaches: true,
+                    clientsClaim: true,
+                },
+
+                devOptions: {
+                    enabled: false,
+                    navigateFallback: 'index.html',
+                    suppressWarnings: true,
+                    type: 'module',
+                },
+            })
+        ],
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src'),
+            },
+        },
+        server: {
+            allowedHosts: env.VITE_LOCAL_DEPLOYMENT_URLS?.split(',').map((host) => host.trim()) || [],
+        },
+    }
 })
